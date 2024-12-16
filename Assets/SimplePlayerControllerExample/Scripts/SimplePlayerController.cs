7eaf29401c1c73b449aef81c8920510c88871096
PradeepGameDev <pradeepjv1820@gmail.com>
2024-12-16 19:31:49 +0530

    Improved player movement added comments

--- Assets/SimplePlayerControllerExample/Scripts/SimplePlayerController.cs
+++ Assets/SimplePlayerControllerExample/Scripts/SimplePlayerController.cs
@@ -11,7 +11,9 @@ public class SimplePlayerController : MonoBehaviour
 
     private PlayerInput _playerInput;
     private Rigidbody _body;
+    private Transform _cameraTransform;
 
+    private Vector3 _moveDirection;
     private Vector3 _targetDirection;
     private Quaternion _targetRotation;
     private Quaternion _playerRotation;
@@ -21,6 +23,7 @@ public class SimplePlayerController : MonoBehaviour
         //Getting component references
         _playerInput = GetComponent<PlayerInput>();
         _body = GetComponent<Rigidbody>();
+        _cameraTransform = Camera.main.transform;
     }
 
     private void FixedUpdate()
@@ -33,10 +36,22 @@ public class SimplePlayerController : MonoBehaviour
 
     private void Movement()
     {
-        _body.linearVelocity = _playerInput.moveInput * moveSpeed;
+        _targetDirection = Vector3.zero;
 
-        _targetDirection = _playerInput.moveInput;
+        //Calculating move direction based on camera forward and player input
+        _moveDirection = _cameraTransform.forward * _playerInput.moveInput.z;
+        _moveDirection += _cameraTransform.right * _playerInput.moveInput.x;
+        _moveDirection.Normalize();
+        _moveDirection.y = 0;
+        _targetDirection = _moveDirection;
 
+        //Changing move direction based on the move speed
+        _moveDirection *= moveSpeed;
+
+        //Applying velocity 
+        _body.linearVelocity = _moveDirection;
+
+        //Checking if target direction is zero, then applying forward direction
         if (_targetDirection == Vector3.zero)
         {
             _targetDirection = transform.forward;
@@ -44,6 +59,7 @@ public class SimplePlayerController : MonoBehaviour
 
         _targetRotation = Quaternion.LookRotation(_targetDirection);
 
+        //Smoothing the transition effect from transform's rotation to new player rotation
         _playerRotation = Quaternion.Slerp(transform.rotation, _targetRotation, rotationSpeed * Time.fixedDeltaTime);
         transform.rotation = _playerRotation;
     }
